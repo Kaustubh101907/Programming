52587ba52ccbecb4965e755a23042e3dd3d7527c
Kaustubh101907 <kaustubhnaithani101010@gmail.com>
2022-05-10 19:35:21 +0530

    learning js

--- JavaScript/home.js
+++ JavaScript/home.js
@@ -1,1 +1,37 @@
 console.log('hello');
+
+// Alert
+// alert('hello my name is kaustubh');
+
+// variables
+var b = 'smoothie';
+console.log(b);
+
+document.getElementById('something').innerHTML = 'Hey There';
+
+// Using Prompt
+// let age = prompt('What is your age?');
+// console.log(age);
+
+// Functions
+function greeting() {
+    let name = prompt('What is your name?');
+    console.log('Hello ' + name);
+}
+
+// greeting();
+
+function sum(a, b) {
+    console.log(a+b);
+}
+
+sum(10, 20);
+
+// Data Types
+let yourAge = 18; // number
+let yourName = 'bob'; // String
+let name = {first: 'Jane', last: 'Doe'}; // object
+let truth = false; // boolean
+let groceries = ['apple', 'banana', 'oranges']; // array
+let random; // undefined
+let nothing = null; 
